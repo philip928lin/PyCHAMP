4698573a6523aec1c4d189293b7990e891ed6a72
Chung-Yi Lin <philip928lin@gmail.com>
2024-04-11 23:41:22 -0400

    Update __init__.py

--- py_champ/__init__.py
+++ py_champ/__init__.py
@@ -1,6 +1,8 @@
 """
 This is the core package for PyCHAMP.
 """
+__version__ = "1.0.0"
+
 # Load functions that directly available for user when the package is loaded.
 from .entities.behavior import Behavior
 from .entities.field import Field
