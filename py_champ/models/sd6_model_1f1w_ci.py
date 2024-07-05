8e2a04a1ddc73e93f6aa1d297f6d5c7997bfabd3
Chung-Yi Lin <philip928lin@gmail.com>
2024-07-05 16:14:13 -0400

    fix field_area

--- py_champ/models/sd6_model_1f1w_ci.py
+++ py_champ/models/sd6_model_1f1w_ci.py
@@ -320,13 +320,14 @@ class SD6Model_1f1w_ci(mesa.Model):
             "yield_rate": get_agt_attr("yield_rate_per_field"),
             "yield": get_agt_attr("y"),  # 1e4 bu/field
             "w": get_agt_attr("w"),
-            "field_area": get_agt_attr("total_field_area"),  # ha
+            "field_area": get_agt_attr("field_area"),  # ha
             "aph_yield_dict": get_agt_attr("aph_yield_dict"),  # a dictionary
             ### Behavior
             "Sa": get_agt_attr("satisfaction"),
             "E[Sa]": get_agt_attr("expected_sa"),
             "Un": get_agt_attr("uncertainty"),
             "state": get_agt_attr("state"),
+            "total_field_area": get_agt_attr("total_field_area"),  # ha
             "gp_status": get_agt_attr("gp_status"),
             "gp_MIPGap": get_agt_attr("gp_MIPGap"),
             ### Finance
